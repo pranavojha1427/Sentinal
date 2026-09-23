31a0d1b71bc0596328f505085c2976a0fc405788
Pranav Ojha <pranavojha1427@gmail.com>
2026-09-23 23:36:57 +0530

    fix: ignore eslint during build to prevent venv files from failing vercel deploy

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 import dns from "node:dns";
 dns.setDefaultResultOrder("ipv4first");
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
